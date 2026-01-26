ca977002c37ceb2da40d886322ca831f17f30599
Arslanlatif <239516961+team-webile@users.noreply.github.com>
2026-01-26 15:37:49 +0500

    Update deploy.sh

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,28 +3,14 @@ set -e
 
 echo "Deployment started ..."
 
-# Enter maintenance mode or return true if already in maintenance mode
-(php artisan down) || true
+php artisan down
 
-# Pull the latest version of the app from main branch
-git pull origin main
+git fetch origin
+git reset --hard origin/main
 
-# Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-
-# Clear old cache
-php artisan clear-compiled
-
-# Recreate cache and optimize
-php artisan optimize
-
-# Compile npm assets
-# npm run prod
-
-# Run database migrations
-# php artisan migrate --force
-
-# Exit maintenance mode
+composer install --no-interaction --prefer-dist --optimize-autoloader
+php artisan migrate --force
+php artisan optimize:clear
 php artisan up
 
 echo "Deployment finished!"
