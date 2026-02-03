58b8b31dc84747ebccfea4e2bc1646461102bb18
Rashid <239516961+team-webile@users.noreply.github.com>
2026-02-03 15:02:38 +0500

    Update ManagerVoterCardController.php

--- app/Http/Controllers/Api/Manager/ManagerVoterCardController.php
+++ app/Http/Controllers/Api/Manager/ManagerVoterCardController.php
@@ -1166,7 +1166,7 @@ class ManagerVoterCardController extends Controller
              
               if ($partyId) {
                   $party = Party::where('name', $partyId)->first();
-                  dd($party->short_name); 
+                  dd($party); 
                   if ($party && isset($party->short_name)) {
                       $partyShortName = strtolower($party->short_name);
                       $query->whereRaw('LOWER(vci.exit_poll) = ?', [$partyShortName]);
