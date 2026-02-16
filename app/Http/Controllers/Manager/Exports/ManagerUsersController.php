84e919f13f8137007b03d56739f093fcbe6d3f79
Rashid <239516961+team-webile@users.noreply.github.com>
2026-02-16 17:51:32 +0500

    Update ManagerUsersController.php

--- app/Http/Controllers/Manager/Exports/ManagerUsersController.php
+++ app/Http/Controllers/Manager/Exports/ManagerUsersController.php
@@ -1216,8 +1216,9 @@ class ManagerUsersController extends Controller
             SELECT DISTINCT ON (voter_id) id 
             FROM surveys
             ORDER BY voter_id, created_at DESC
-        ) AS latest_surveys"), 'surveys.id', '=', 'latest_surveys.id', 'surveys.cell_phone_code', 'surveys.cell_phone')
-        ->select('voters.*', 'constituencies.name as constituency_name', 'surveys.id as survey_id', 'surveys.created_at as survey_date', 'surveys.user_id', 'surveys.voting_for')
+        ) AS latest_surveys"), 'surveys.id', '=', 'latest_surveys.id')
+        ->select('voters.*', 'constituencies.name as constituency_name', 'surveys.id as survey_id',
+         'surveys.created_at as survey_date', 'surveys.user_id', 'surveys.voting_for','surveys.cell_phone','surveys.cell_phone_code')
         ->leftJoin('constituencies', 'voters.const', '=', 'constituencies.id')
         ->where(function($query) {
             $query->whereNull('surveys.voting_for')->where('surveys.voting_decision','undecided');
