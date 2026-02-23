9280248299119f73ef735f340fe1246caa4a67c1
Rashid <239516961+team-webile@users.noreply.github.com>
2026-02-23 14:07:19 +0500

    Update VoterController.php

--- app/Http/Controllers/Admin/Exports/VoterController.php
+++ app/Http/Controllers/Admin/Exports/VoterController.php
@@ -1305,7 +1305,7 @@ class VoterController extends Controller
             return strtolower(urldecode(trim($column)));
         }, explode(',', $_GET['columns']));
 
-        dd($columns);
+        dd($columns,$callCenters); 
        $timestamp = now('America/New_York')->format('Y-m-d_g:iA');
        return Excel::download(new CallCenterExport($callCenters, $request, $columns), 'Call_Center_' . $timestamp . '.xlsx'); 
 
